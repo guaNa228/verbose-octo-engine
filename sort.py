de80d8060e84fdb400a043af1d101b9e7828adc4
G <frolovgeorgiy@gmail.com>
2022-11-13 18:40:30 +0300

    Removing bubble, adding quicksort

--- sort.py
+++ sort.py
@@ -1,11 +1,28 @@
-def bubbleSort(array):
-    
-  for i in range(len(array)):
+def partition(array, start, end):
+    pivot = array[start]
+    low = start + 1
+    high = end
 
-    for j in range(0, len(array) - i - 1):
+    while True:
+        while low <= high and array[high] >= pivot:
+            high = high - 1
 
-      if array[j] > array[j + 1]:
+        while low <= high and array[low] <= pivot:
+            low = low + 1
 
-        temp = array[j]
-        array[j] = array[j+1]
-        array[j+1] = temp
+        if low <= high:
+            array[low], array[high] = array[high], array[low]
+        else:
+            break
+
+    array[start], array[high] = array[high], array[start]
+
+    return high
+
+def quick_sort(array, start, end):
+    if start >= end:
+        return
+
+    p = partition(array, start, end)
+    quick_sort(array, start, p-1)
+    quick_sort(array, p+1, end)
