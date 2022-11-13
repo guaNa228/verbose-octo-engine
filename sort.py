7275ca6f53fc49a4a1b43b895e9b7d261fb85c3b
K-ate <katya.kovrigina.2016@mail.ru>
2022-11-13 18:28:35 +0300

    adding Shell sort

--- sort.py
+++ sort.py
@@ -1,11 +1,18 @@
-def bubbleSort(array):
-    
-  for i in range(len(array)):
-
-    for j in range(0, len(array) - i - 1):
-
-      if array[j] > array[j + 1]:
-
-        temp = array[j]
-        array[j] = array[j+1]
-        array[j+1] = temp
+def shellSort(arr):
+ 
+    n = len(arr)
+    gap = n/2
+ 
+    while gap > 0:
+ 
+        for i in range(gap,n):
+ 
+            temp = arr[i]
+ 
+            j = i
+            while  j >= gap and arr[j-gap] >temp:
+                arr[j] = arr[j-gap]
+                j -= gap
+ 
+            arr[j] = temp
+        gap /= 2
